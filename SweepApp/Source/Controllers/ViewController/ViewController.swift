35dd9c81e6ff1f6dfdba584e7c495f9b72b37af2
Joan Wilson <joanwmoliveira@gmail.com>
2022-09-27 09:51:21 -0300

    solve merge conflicts

--- SweepApp/Source/Controllers/ViewController/ViewController.swift
+++ SweepApp/Source/Controllers/ViewController/ViewController.swift
@@ -4,12 +4,13 @@
 //
 //  Created by Nicolas Barbosa on 09/09/22.
 //  swiftlint:disable trailing_whitespace
+//  swiftlint:disable line_length
 
 import UIKit
 import CoreData
 
 final class ViewController: UIViewController {
-    
+
     fileprivate var viewModel = ViewModel() {
         didSet {
             print(viewModel.getUncompletedArray().count)
@@ -40,23 +41,23 @@ final class ViewController: UIViewController {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        
+
         return imageView
     }()
-    
+
     public lazy var attackAnimation: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        
+
         return imageView
     }()
-    
+
     public lazy var zombieCharacter: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         return imageView
     }()
 
@@ -82,12 +83,12 @@ final class ViewController: UIViewController {
         button.setTitle("Licenças", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 10)
         button.alignTextBelow()
-        
+
         button.translatesAutoresizingMaskIntoConstraints = false
 
         return button
     }()
-    
+
     fileprivate lazy var historyButton: UIButton = {
         let button = UIButton()
         button.setImage(
@@ -103,10 +104,10 @@ final class ViewController: UIViewController {
         button.titleLabel?.font = UIFont.systemFont(ofSize: 10)
         button.alignTextBelow()
         button.translatesAutoresizingMaskIntoConstraints = false
-        
+
         return button
     }()
-    
+
     fileprivate lazy var addButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setImage(
@@ -123,7 +124,7 @@ final class ViewController: UIViewController {
         button.alignTextBelow()
 
         button.translatesAutoresizingMaskIntoConstraints = false
-        
+
         return button
     }()
 
@@ -132,25 +133,25 @@ final class ViewController: UIViewController {
         imageView.image = UIImage(named: "background-2")
         imageView.contentMode = .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         return imageView
     }()
-    
+
     fileprivate lazy var tasksCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.itemSize = CGSize(width: view.bounds.width*0.3, height: view.bounds.height*0.1)
         layout.sectionInset = UIEdgeInsets(top: 0, left: 3, bottom: 0, right: 10)
-        
+
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.setCollectionViewLayout(layout, animated: true)
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = .clear
-        
+
         return collectionView
     }()
-    
+
     fileprivate lazy var imageCollectionViewBackground: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "asphalt")
@@ -158,13 +159,13 @@ final class ViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isToolbarHidden = false
         buildLayout()
     }
-    
+
     @objc public func showAddTaskView() {
         let addTaskViewController = AddTaskViewController()
         addTaskViewController.modalPresentationStyle = .overFullScreen
@@ -172,7 +173,7 @@ final class ViewController: UIViewController {
         addTaskViewController.modalTransitionStyle = .crossDissolve
         self.present(addTaskViewController, animated: true)
     }
-    
+
     @objc private func showHistoryView() {
         let historyViewController = HistoryViewController()
         historyViewController.modalPresentationStyle = .overFullScreen
@@ -214,7 +215,7 @@ extension ViewController: AddTaskWindowDelegate {
     func addANewTask(nameTask: String) {
         viewModel.service.addATask(for: nameTask)
         viewModel.setTaskArray()
-        
+
     }
 }
 
@@ -226,11 +227,11 @@ extension ViewController: ViewCoding {
             TasksCollectionViewCell.self,
             forCellWithReuseIdentifier: TasksCollectionViewCell.identifier
         )
-        
+
         self.startAnimations()
 
     }
-    
+
     func setupConstraints() {
 
         NSLayoutConstraint.activate([
@@ -238,7 +239,7 @@ extension ViewController: ViewCoding {
             imageBackground.bottomAnchor.constraint(equalTo: imageCollectionViewBackground.topAnchor),
             imageBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imageBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
+
             tasksCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tasksCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tasksCollectionView.bottomAnchor.constraint(
@@ -246,17 +247,17 @@ extension ViewController: ViewCoding {
                 constant: -view.bounds.height*0.075
             ),
             tasksCollectionView.topAnchor.constraint(equalTo: imageCollectionViewBackground.topAnchor),
-            
+
             imageCollectionViewBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imageCollectionViewBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             imageCollectionViewBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             imageCollectionViewBackground.heightAnchor.constraint(equalToConstant: view.bounds.height*0.3),
-            
+
             addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             addButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             addButton.heightAnchor.constraint(equalToConstant: view.bounds.height*0.1),
             addButton.widthAnchor.constraint(equalToConstant: view.bounds.height*0.15),
-            
+
             historyButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             historyButton.trailingAnchor.constraint(
                 equalTo: addButton.leadingAnchor,
@@ -269,7 +270,7 @@ extension ViewController: ViewCoding {
             licenseButton.widthAnchor.constraint(equalToConstant: view.bounds.height*0.15),
             licenseButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             licenseButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            
+
             marcoCharacter.heightAnchor.constraint(equalToConstant: 440),
             marcoCharacter.widthAnchor.constraint(equalToConstant: 216),
             marcoCharacter.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.bounds.height*0.08),
@@ -277,7 +278,7 @@ extension ViewController: ViewCoding {
                 equalTo: view.safeAreaLayoutGuide.leadingAnchor,
                 constant: view.bounds.width*0.28
             ),
-            
+
             attackAnimation.heightAnchor.constraint(equalToConstant: 440),
             attackAnimation.widthAnchor.constraint(equalToConstant: 216),
             attackAnimation.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.bounds.height*0.08),
@@ -285,7 +286,7 @@ extension ViewController: ViewCoding {
                 equalTo: view.safeAreaLayoutGuide.leadingAnchor,
                 constant: view.bounds.width*0.28
             ),
-            
+
             zombieCharacter.heightAnchor.constraint(equalToConstant: 105),
             zombieCharacter.widthAnchor.constraint(equalToConstant: 60),
             zombieCharacter.bottomAnchor.constraint(
@@ -302,7 +303,7 @@ extension ViewController: ViewCoding {
 
         ])
     }
-    
+
     func setupHierarchy() {
         view.addSubview(imageBackground)
         view.addSubview(imageCollectionViewBackground)
@@ -315,7 +316,7 @@ extension ViewController: ViewCoding {
         view.addSubview(licenseButton)
         view.addSubview(noTaskDetectedLabel)
     }
-    
+
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -332,46 +333,56 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         }
 
     }
-    
-    public func collectionView(
-        _ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
 
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: TasksCollectionViewCell.identifier,
-            for: indexPath
-        ) as? TasksCollectionViewCell else {
-            return UICollectionViewCell()
-            
-        }
-        let favorite = UIAction(title: "Completar",
-                                image: UIImage(systemName: "checkmark.square")) { _ in
+    func displayAlert(task: Task) {
+
+        // Declare Alert message
+        let dialogMessage = UIAlertController(title: task.name, message: "Are you sure you want to delete this?", preferredStyle: .alert) //teste
+
+        // Create Cancel button with action handlder
+        let cancel = UIAlertAction(title: "Cancelar", style: .cancel) { _ -> Void in print("cancelado")}
+
+        // Create OK button with action handler
+        let finish = UIAlertAction(title: "Finalizar", style: .default, handler: { _ -> Void in
             DispatchQueue.main.async {
-                let task = self.viewModel.getUncompletedArray()[indexPath.row]
+                let task = task
                 self.viewModel.service.completeATask(task)
                 self.viewModel.setTaskArray()
                 self.doAnimate()
-                print(indexPath)
-
             }
+        })
+
+        // Add OK and Cancel button to dialog message
+        dialogMessage.addAction(cancel)
+        dialogMessage.addAction(finish)
+
+        // Present dialog message to user
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+
+    // ====================================================================================================================================================================
+    //                                                             VISUALIZAÇÃO DAS TELAS E DAS TAKS PRINCIPAIS
+    // ====================================================================================================================================================================
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TasksCollectionViewCell.identifier, for: indexPath) as? TasksCollectionViewCell else {
+            return UICollectionViewCell()
         }
 
         cell.taskLabel.text = viewModel.getUncompletedArray()[indexPath.row].name
-        cell.buttonMenu.menu = UIMenu(
-            title: "\(viewModel.getUncompletedArray()[indexPath.row].name!)",
-            children: [favorite]
-        )
+        cell.buttonMenu.isHidden = true
 
         return cell
     }
-    
-    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let task = viewModel.getUncompletedArray()[indexPath.row]
+        displayAlert(task: task)
     }
 
     func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
         return true
     }
-    
+
 }
